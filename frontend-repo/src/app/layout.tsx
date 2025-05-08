61d1353b966d8f1fdaf100c4e7fe59ba6c41c74b
HzdAngga <angga220801@gmail.com>
2025-05-09 06:48:25 +0700

    refactor structure

--- frontend-repo/src/app/layout.tsx
+++ frontend-repo/src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
 import { ThemeProvider } from "@mui/material";
-import theme from "@/libs/mui/theme";
+import theme from "@/theme/mui-theme";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
