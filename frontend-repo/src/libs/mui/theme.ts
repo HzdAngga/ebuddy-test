148d800988f2cfa0a18aeb62ad73f8f70dab5411
HzdAngga <angga220801@gmail.com>
2025-05-09 06:44:51 +0700

    integrate with mui

---
+++ frontend-repo/src/libs/mui/theme.ts
@@ -0,0 +1,11 @@
+"use client";
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  // write custom theme here
+  typography: {
+    fontFamily: "var(--font-geist-sans)",
+  },
+});
+
+export default theme;
